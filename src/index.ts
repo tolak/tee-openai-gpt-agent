7247f8c6f2fa360cfea4c909d007bea59972cb56
Wenfeng Wang <kalot.wang@gmail.com>
2024-09-09 06:50:24 +0000

    request openai API with the given key

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import OpenAI from 'openai'
 
 async function GET(req: Request): Promise<Response> {
     let result = { message: '' }
-    const secrets = req.secret || {}
     const queries = req.queries
-    const openaiApiKey = (secrets.openaiApiKey) ? secrets.openaiApiKey as string : ''
-    const openai = new OpenAI({ apiKey: openaiApiKey })
+    const openaiApiKey = queries.openaiApiKey ? queries.openaiApiKey[0] as string : undefined;
+    const openai = new OpenAI({
+        apiKey: openaiApiKey,
+        baseURL: 'https://faraday.cloud/v1'
+    });
     // Choose from any model listed here https://platform.openai.com/docs/models
-    const openAiModel = (queries.openAiModel) ? queries.openAiModel[0] : 'gpt-4o';
+    const openAiModel = (queries.openAiModel) ? queries.openAiModel[0] : 'gpt-4o-mini';
     const query = (queries.chatQuery) ? queries.chatQuery[0] as string : 'Who are you?'
 
     const completion = await openai.chat.completions.create({
